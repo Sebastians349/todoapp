333910b599a8452fc34a4ad899ec33b1f69679c6
Sebastian Simarro <ssebastian210@gmail.com>
2021-10-23 09:29:08 -0300

    <push><init>

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">YET ANOTHER TO DO LIST....</h1>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">List of todos</h4>
+          <ul className="list-group"></ul>
+        </div>
+        <div
+          className="col-4
+        "
+        >
+          <h4 className="text-center">Add todo</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
